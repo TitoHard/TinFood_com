fb5230680f262571031fc66321c7b6cd65467b18
TitoHard <147843387+TitoHard@users.noreply.github.com>
2023-10-13 12:05:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TinFood_com
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/TitoHard/TinFood_com)
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+
+<header>
+
+</header>
+
+<main>
+
+</main>
+<style>
+
+</style>
